a3958fc09979c5310829e2cfa4c633fd6b475235
SaiGovardhana <191fa04292@gmail.com>
2022-08-20 22:12:39 +0530

    Modified heading

---
+++ src/Heading/BottomText.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function BottomText()
+{
+    return (<h4 className="bottom-text">A SIMPLE WORD GUESSING GAME</h4>)
+}
--- src/Heading/WordleDisplayer.jsx
+++ src/Heading/WordleDisplayer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import BottomText from "./BottomText";
 import WordleLetter from "./WordleLetter";
 
 export default function WordleDisplayer()
 {
     let letters=['W','O','R','D','L','E']
     let colors=['rgb(0,255,4)','rgb(255,0,0)','rgb(255,159,26)','rgb(154,155,147)']
-    return (<div className="wordle-display-container">
+    return (
+    <>
+            <div className="wordle-display-container">
         
-        {letters.map((x,i)=><WordleLetter key={i} text={x} bgColor={colors[i%colors.length]}></WordleLetter>)}    
-
-        </div>)
+                {letters.map((x,i)=><WordleLetter key={i} text={x} bgColor={colors[i%colors.length]}></WordleLetter>)}    
+            </div>
+            <BottomText></BottomText>
+    </>   
+            )
 }
